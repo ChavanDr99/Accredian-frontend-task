35983d32006d1773659b57231e30079f6656d4b0
ChavanDr99 <chavandipesh399@gmail.com>
2023-12-19 15:31:06 +0530

    final commit

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -5,6 +5,7 @@ import { Link} from 'react-router-dom'
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios'
 import {toast} from 'react-hot-toast';
+// import axios from 'axios'
 
 const Login = () => {
   const navigate=useNavigate();
